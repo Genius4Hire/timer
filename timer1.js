f8edd82a194f8ba80c4bd688d46de0468435a91a
Jordan Dennis <jordan.dennis@gmail.com>
2023-06-07 03:47:57 +0000

    Refactoring.

--- timer1.js
+++ timer1.js
@@ -4,17 +4,18 @@ timers.shift();
 timers.shift();
 
 //define beep as a scheduled "setTimeOut" output of  "·"
-const beep = (time, callback) => {
+const beep = (time) => {
+  // schedule a 'beep'...
   setTimeout(() => {
     process.stdout.write("·");
-  },time);
+  },time); // ..in 'time' milliseconds
 };
 
-// list the timers
-// console.log(timers);
+// for each timer defined in the command line...
 for (timer of timers) {
+  // ..and, making sure the timer is a proper number..
   if (timer !== NaN) {
-    // console.log("Beep scheduled!")
+    // schedule that beep for the number of seconds defined in this particular timer
     if (timer > 0) beep(timer * 1000);   
   }
 };
